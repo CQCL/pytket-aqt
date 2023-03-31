52008545f7880cf687c0f71a8df5d1bb7e799cef
melf <cqc@melf.de>
2023-03-31 12:56:03 +0100

    update python 3.8 to 3.9, might work

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     version=metadata["__extension_version__"],
     author="TKET development team",
     author_email="tket-support@cambridgequantum.com",
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     project_urls={
         "Documentation": "https://cqcl.github.io/pytket-aqt/api/index.html",
         "Source": "https://github.com/CQCL/pytket-aqt",
@@ -44,7 +44,7 @@ setup(
     install_requires=["pytket ~= 1.13.2", "requests ~= 2.22", "types-requests"],
     classifiers=[
         "Environment :: Console",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: Apache Software License",
