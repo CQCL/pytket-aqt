ee6226a23a12a9d24a6cabbb8e514e8a274590c2
Melf <cqc@melf.de>
2023-05-10 11:08:17 +0100

    update pytket verion

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.14",
+        "pytket == 1.15.0rc1",
         "requests ~= 2.22",
         "types-requests",
         "pydantic ~= 1.10",
