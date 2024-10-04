60a47d8f388eb5127ef43147e221c992dbea136d
Melf <cqc@melf.de>
2024-10-04 16:00:08 +0100

    ruff changes 5

--- pytket/extensions/aqt/__init__.py
+++ pytket/extensions/aqt/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Backends for processing pytket circuits with AQT devices."""
+
 from .backends import AQTBackend
 from .backends.config import (
     AQTAccessToken,
