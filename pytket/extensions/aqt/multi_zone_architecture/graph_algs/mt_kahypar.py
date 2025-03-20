2ef1a9dd2bd932b70061381b6148fde5380cc17d
Travis Thompson <travis.thompson@quantinuum.com>
2025-03-20 15:52:20 +0100

    align with mtkahypar api changes

--- pytket/extensions/aqt/multi_zone_architecture/graph_algs/mt_kahypar.py
+++ pytket/extensions/aqt/multi_zone_architecture/graph_algs/mt_kahypar.py
@@ -11,19 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import mtkahypar  # type: ignore
 
 from pytket.extensions.aqt.multi_zone_architecture.graph_algs.graph import GraphData
 
-
-def graph_data_to_mtkahypar_graph(graph_data: GraphData) -> mtkahypar.Graph:
-    return mtkahypar.Graph(
-        graph_data.n_vertices,
-        len(graph_data.edges),
-        graph_data.edges,
-        graph_data.vertex_weights,
-        graph_data.edge_weights,
-    )
+mtkahypar_nthreads = 1
+mtk = mtkahypar.initialize(mtkahypar_nthreads)
 
 
 class MtKahyparPartitioner:
@@ -35,12 +29,31 @@ class MtKahyparPartitioner:
     """
 
     def __init__(self, n_threads: int, log_level: int = 0):
-        mtkahypar.initialize(n_threads)
-        mtkahypar.setSeed(13)
-        self.context = mtkahypar.Context()
-        self.context.loadPreset(mtkahypar.PresetType.DEFAULT)
+        mtkahypar.set_seed(13)
+        self.context = mtk.context_from_preset(mtkahypar.PresetType.DEFAULT)
         self.context.logging = False
         self.log_level = log_level
+
+    def graph_data_to_mtkahypar_graph(self, graph_data: GraphData) -> mtkahypar.Graph:
+        return mtk.create_graph(
+            self.context,
+            graph_data.n_vertices,
+            len(graph_data.edges),
+            graph_data.edges,
+            graph_data.vertex_weights,
+            graph_data.edge_weights,
+        )
+
+    def graph_data_to_mtkahypar_target_graph(
+        self, graph_data: GraphData
+    ) -> mtkahypar.TargetGraph:
+        return mtk.create_target_graph(
+            self.context,
+            graph_data.n_vertices,
+            len(graph_data.edges),
+            graph_data.edges,
+            graph_data.edge_weights,
+        )
 
     def partition_graph(
         self,
@@ -55,22 +68,22 @@ class MtKahyparPartitioner:
         :param num_parts: Number of partitions
         """
         avg_part_weight = sum(graph_data.vertex_weights) / num_parts
-        self.context.setPartitioningParameters(
+        self.context.set_partitioning_parameters(
             num_parts,
             0.5 / avg_part_weight,
             mtkahypar.Objective.CUT,
         )
-        graph = graph_data_to_mtkahypar_graph(graph_data)
+        graph = self.graph_data_to_mtkahypar_graph(graph_data)
         if graph_data.fixed_list:
-            graph.addFixedVertices(graph_data.fixed_list, num_parts)
+            graph.add_fixed_vertices(graph_data.fixed_list, num_parts)
         if graph_data.part_max_sizes:
-            self.context.max_block_weights = graph_data.part_max_sizes
+            self.context.set_individual_target_block_weights(graph_data.part_max_sizes)
         part_graph = graph.partition(self.context)
         if self.log_level > 0:
             print("cut_cost: ", part_graph.cut())  # noqa: T201
         vertex_part_id: list[int] = []
         for vertex in range(graph_data.n_vertices):
-            vertex_part_id.append(part_graph.blockID(vertex))
+            vertex_part_id.append(part_graph.block_id(vertex))
         return vertex_part_id
 
     def map_graph_to_target_graph(
@@ -83,17 +96,17 @@ class MtKahyparPartitioner:
          graph node that vertex i is assigned to
         """
         avg_part_weight = sum(graph_data.vertex_weights) / target_graph_data.n_vertices
-        self.context.setPartitioningParameters(
+        self.context.set_partitioning_parameters(
             target_graph_data.n_vertices,
             0.5 / avg_part_weight,
             mtkahypar.Objective.CUT,  # This doesn't matter, Steiner tree metric is used
         )
-        graph = graph_data_to_mtkahypar_graph(graph_data)
+        graph = self.graph_data_to_mtkahypar_graph(graph_data)
         if graph_data.part_max_sizes:
-            self.context.max_block_weights = graph_data.part_max_sizes
-        target_graph = graph_data_to_mtkahypar_graph(target_graph_data)
-        part_graph = graph.mapOntoGraph(target_graph, self.context)
+            self.context.set_individual_target_block_weights(graph_data.part_max_sizes)
+        target_graph = self.graph_data_to_mtkahypar_target_graph(target_graph_data)
+        part_graph = graph.map_onto_graph(target_graph, self.context)
         vertex_part_id: list[int] = []
         for vertex in range(graph_data.n_vertices):
-            vertex_part_id.append(part_graph.blockID(vertex))
+            vertex_part_id.append(part_graph.block_id(vertex))
         return vertex_part_id
