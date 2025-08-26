f0cabbbdc849a8b55804ac937f418852328d6b05
Will Simmons <will.simmons@cambridgequantum.com>
2025-08-26 11:43:22 +0100

    Copy workflows and docs build from other extensions (#151)
    * Copy workflows and docs build from other extensions
    
    * Add test requirements
    
    * Just add pytest-httpx to test requirements to account for qiskit-aqt-provider
    
    * Import assert_never from typing-extensions so it is available for python 3.10
    
    * Split imports and noqa for python 3.10
    
    * Forgot to copy mypy-check over from other repos
    
    * </facepalm> forgot to copy over mypy.ini
    
    * Mypy doesn't find pytket.circuit stuff from pytket
    
    * Remove python 3.10; run pre-commit reformatting
    
    * My initial attempt at recreating the pre-commit github action
    
    * Fix typo in setup-python versioning
    
    * Another typo; should have just copy pasted
    
    * Try poetry sync first
    
    * A commit that should fail the CI pre-commit; will revert
    
    * Revert "A commit that should fail the CI pre-commit; will revert"
    
    This reverts commit 7d0065914a510ad980832513f97041e312c8ebbf.
    
    * Remove nox completely and add mypy check to CI pre-commit
    
    * Need poetry to install more packages for mypy
    
    * My trial-and-error-style is a little too much on the error side
    
    * Remove unnecessary stuff

--- pytket/extensions/aqt/multi_zone_architecture/depth_list/depth_list.py
+++ pytket/extensions/aqt/multi_zone_architecture/depth_list/depth_list.py
@@ -13,8 +13,7 @@
 # limitations under the License.
 from typing import TypeAlias
 
-from pytket import Circuit, Qubit
-from pytket._tket.circuit import OpType
+from pytket.circuit import Circuit, OpType, Qubit
 
 DepthList: TypeAlias = list[list[tuple[int, int]]]  # noqa: UP040
 
